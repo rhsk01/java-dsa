ba22a01ac9f02fc80f43484eabd1af40eada9cd3
Raja Rishabh Singh <rajarishabhsingh2001@gmail.com>
2022-06-26 21:13:50 +0530

    sll practice

--- array.java
+++ array.java
@@ -2,12 +2,14 @@ package com.company;
 
 public class array {
     public static void main(String[] args) {
-        int[] arr = new int[5];
+        int[] arr = new int[7];
         arr[0] = 1;
         arr[1] = 2;
         arr[2] = 3;
         arr[3] = 4;
         arr[4] = 5;
+        arr[5] = 6;
+        arr[6] = 7;
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
